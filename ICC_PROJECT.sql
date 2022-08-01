de210afb0ba4b92cca2b04c7eb8db97d97c70bac
PrajwalithaGalanki <110342264+PrajwalithaGalanki@users.noreply.github.com>
2022-08-01 12:14:52 +0530

    Edited spacing

--- ICC_PROJECT.sql
+++ ICC_PROJECT.sql
@@ -1,64 +1,64 @@
 create database ICC;
 use ICC;
 create table players(
-                    player_id varchar(7) not null, 
-					player_name varchar(20) not null ,
-					DoB date ,
-					age integer,
-                    country varchar(20),
-                    primary key (player_id)
+                    	player_id varchar(7) not null, 
+			player_name varchar(20) not null ,
+			DoB date ,
+			age integer,
+        	        country varchar(20),
+                    	primary key (player_id)
                     );
-					insert into players values('IND0001','KOHLI','1988-11-5',33,'India');
+		    insert into players values('IND0001','KOHLI','1988-11-5',33,'India');
                     insert into players values('AUS0001','FINCH','1986-11-17',35,'Australia');
                     insert into players values('NZ0001','WILLIAMSON','1990-8-8',31,'Newzealand');
                     insert into players values('ENG0001','MORGAN','1986-9-10',35,'England');
                     insert into players values('SA0001','FAfDUPLESIE','1984-7-13',37,'South Africa');
-					insert into players values('SL0001','JAYAVARDHENE','1977-5-27',44,'Sri Lanka');
-					insert into players values('PAK0001','BABAR AZAM','1994-10-15',27,'Pakistan');
+		    insert into players values('SL0001','JAYAVARDHENE','1977-5-27',44,'Sri Lanka');
+		    insert into players values('PAK0001','BABAR AZAM','1994-10-15',27,'Pakistan');
                     insert into players values('IND0002','BHUVANESHWAR','1990-2-5',32,'India');
                     insert into players values('AUS0002','STARC','1990-1-30',32,'Australia');
                     insert into players values('NZ0002','BOULT','1989-7-22',32,'Newzealand');
                     insert into players values('ENG0002','ARCHER','1995-4-1',27,'England');
                     insert into players values('SA0002','RABADA','1995-5-25',27,'South Africa');
-					insert into players values('SL0002','MALINGA','1983-8-28',38,'Sri Lanka');
+		    insert into players values('SL0002','MALINGA','1983-8-28',38,'Sri Lanka');
                     insert into players values('PAK0002','AFRIDI','1980-3-1',42,'Pakistan');
                     insert into players values('IND0003','JEDAJA','1988-12-6',33,'India');
                     insert into players values('AUS0003','CUMMINS','1993-5-8',29,'Australia');
                     insert into players values('NZ0003','GRANDHOME','1986-7-22',35,'Newzealand');
                     insert into players values('ENG0003','SAMCURRAN','1998-6-3',24,'England');
                     insert into players values('SA0003','NGIDI','1996-3-29',26,'South Africa');
-					insert into players values('SL0003','HASARANGA','1997-4-29',25,'Sri Lanka');
-					insert into players values('PAK0003','SHOAIB MALIK','1982-2-1',40,'Pakistan');
+		    insert into players values('SL0003','HASARANGA','1997-4-29',25,'Sri Lanka');
+		    insert into players values('PAK0003','SHOAIB MALIK','1982-2-1',40,'Pakistan');
                     insert into players values('IND0004','RISHABH','1997-4-10',24, 'India');
                     insert into players values('AUS0004','CAREY','1991-8-27',30,'Australia');
                     insert into players values('NZ0004','McCullum','1981-9-27',40,'NewZealand');
-					insert into players values('SL0004','SANGAKARA','1977-10-27',44,'Sri Lanka');
+		    insert into players values('SL0004','SANGAKARA','1977-10-27',44,'Sri Lanka');
 create table batsmen(
                     player_id char(7) primary key,
                     TEST_strike_rate float,
                     ODI_strike_rate float,
                     T20_strike_rate float,
-					TEST_average float,
+		    TEST_average float,
                     ODI_average float,
                     T20_average float,
                     toatl_runs integer(8),
                     TEST_ranking integer(4),
-					ODI_ranking integer(4),
-					T20_ranking integer(4),
+		    ODI_ranking integer(4),
+		    T20_ranking integer(4),
                     best integer(3),
                     foreign key (player_id) references players (player_id)
-);
+		    );
                     insert into batsmen values('IND0001',55.7,92.9,137.7,49.5,57.9,50.1,3674832,4,2,1,183);
-					insert into batsmen values('NZ0001',51.2,81.8,124.0,52.6,47.5,32.6,2345452,1,2,3,120);
+		    insert into batsmen values('NZ0001',51.2,81.8,124.0,52.6,47.5,32.6,2345452,1,2,3,120);
                     insert into batsmen values('AUS0001',45.0,88.0,145.3,27.8,40.4,35.2,5674332,2,3,3,100);
                     insert into batsmen values('ENG0001',64.1,86.0,149.2,24.7,28.2,13.8,1234232,3,5,5,200);
                     insert into batsmen values('PAK0001',53.5,90.2,129.4,47.3,59.2,45.5,956234,6,4,2,178);
                     insert into batsmen values('SL0001',51.5,79.0,133.2,49.8,33.4,39.8,979839,50,14,22,121);
 create table bowler(
                     player_id char(7) primary key,
-					wickets integer(5),
-					TEST_ranking integer(3),
-					ODI_ranking integer(3),
+		    wickets integer(5),
+	            TEST_ranking integer(3),
+		    ODI_ranking integer(3),
                     T20_ranking integer(4),
                     TEST_average float,
                     ODI_average float,
@@ -76,47 +76,47 @@ create table bowler(
                     
 create table allrounder(
                         player_id char(7) primary key,
-						ranking integer(5),
-						toatl_runs integer(8),
+			ranking integer(5),
+			toatl_runs integer(8),
                         TEST_strike_rate float,
                         ODI_strike_rate float,
                         T20_strike_rate float,
-					    TEST_batting_average float,
+			TEST_batting_average float,
                         ODI_batting_average float,
                         T20_batting_average float,
-						TEST_batting_ranking integer(4),
-						ODI_batting_ranking integer(4),
-						T20_batting_ranking integer(4),
-						best_batting integer(3),
-						wickets integer(5),
-						TEST_bowling_ranking integer(3),
-						ODI_bowling_ranking integer(3),
-						T20_bowling_ranking integer(4),
+			TEST_batting_ranking integer(4),
+			ODI_batting_ranking integer(4),
+			T20_batting_ranking integer(4),
+			best_batting integer(3),
+			wickets integer(5),
+			TEST_bowling_ranking integer(3),
+			ODI_bowling_ranking integer(3),
+			T20_bowling_ranking integer(4),
                         TEST_average float,
                         ODI_average float,
                         T20_average float,
-						economy float,
-						style char(4),
-						best_bowling char(8),
+			economy float,
+			style char(4),
+			best_bowling char(8),
                         foreign key (player_id) references players (player_id)
                         );
                         insert into allrounder values('IND0003',2,5363,59.5,86.7,128.5,36.6,32.5,23.7,10,7,5,175,453,11,6,8,24.71,37.36,27.91,7.56,'spin','7 for 48');
                         insert into allrounder values('ENG0003',4,3186,64.12,85.97,149.23,24.69,28.2,13.85,8,9,15,94,202,2,3,4,35.51,36.50,25.56,7.30,'seam','7 for 58');
-						insert into allrounder values('NZ0003',7,6592,80.44,106.15,138.35,38.7,26.5,15.78,18,7,22,120,205,15,22,17,32.95,41.00,38.41,6.5,'seam','6 for 24');
+			insert into allrounder values('NZ0003',7,6592,80.44,106.15,138.35,38.7,26.5,15.78,18,7,22,120,205,15,22,17,32.95,41.00,38.41,6.5,'seam','6 for 24');
                         insert into allrounder values('AUS0003',1,1214,40.81,73.97,130.76,16.92,10.12,7.55,82,245,41,52,63,4,12,8,21.66,28.04,22.77,5.6,'seam','9 for 62');
                         insert into allrounder values('SL0003',8,2323,44.81,83.88,149.54,18.55,18.22,8.45,76,133,74,32,42,5,17,9,23.78,29.94,21.67,5.4,'seam','8 for 89');
                         insert into allrounder values('PAK0003',15,6592,64.12,85.97,130.76,16.92,23.7,7.55,67,145,8,19,62,61,32,3,23.78,23.94,22.67,6.4,'seam','8 for 93');
                         
 create table batsmen_vs_bowler(
-                        batsmen_id varchar(7) not null,
-					    bowler_id varchar(7) not null,
-					    matches_played int(3),
-						runs_scored int(3),
-						dismissals int(2),
-                        foreign key (batsmen_id) references players (player_id),
-						foreign key (bowler_id) references players (player_id)
-                        );
-						insert into batsmen_vs_bowler values('AUS0001','ENG0002',20,97,4);
+                        	batsmen_id varchar(7) not null,
+				bowler_id varchar(7) not null,
+				matches_played int(3),
+				runs_scored int(3),
+				dismissals int(2),
+				foreign key (batsmen_id) references players (player_id),
+				foreign key (bowler_id) references players (player_id)
+                        	);
+			insert into batsmen_vs_bowler values('AUS0001','ENG0002',20,97,4);
                         insert into batsmen_vs_bowler values('AUS0001','SA0003',19,123,0);
                         insert into batsmen_vs_bowler values('SL0003','IND0002',18,47,5);
                         insert into batsmen_vs_bowler values('ENG0001','IND0002',17,23,4);
@@ -138,26 +138,26 @@ create table keeper(
                    stumpings integer(4),
                    foreign key (keeper_id) references players (player_id)
                    );
-				   insert into keeper values('IND0004',3,120,75);
-				   insert into keeper values('AUS0004',7,156,87);
-				   insert into keeper values('NZ0004',9,170,68);
+		   insert into keeper values('IND0004',3,120,75);
+		   insert into keeper values('AUS0004',7,156,87);
+		   insert into keeper values('NZ0004',9,170,68);
                    insert into keeper values('SL0004',5,188,103);
                    
 create table ranking(
                     country varchar(20),
-					game_type varchar(4),
+		    game_type varchar(4),
                     former_captain_id varchar(7),
-					country_rank integer(2),
+		    country_rank integer(2),
                     batsman_top_rank integer(3),
                     bowler_top_rank integer(3),
                     allrounder_top_rank integer(3),
                     primary key(country,game_type),
                     foreign key (former_captain_id) references players(player_id)
                     );
-					insert into ranking values('INDIA','TEST','IND0001',2,2,6,1);
+		    insert into ranking values('INDIA','TEST','IND0001',2,2,6,1);
                     insert into ranking values('AUSTRALIA','TEST','AUS0001',4,1,8,2);
                     insert into ranking values('ENGLAND','TEST','ENG0003',3,3,8,3);
-					insert into ranking values('NEWZLAND','TEST','NZ0001',1,4,1,7);
+		    insert into ranking values('NEWZLAND','TEST','NZ0001',1,4,1,7);
                     insert into ranking values('SOUTHAFRICA','TEST','SA0001',5,5,8,6);
                     insert into ranking values('PAKISTAN','TEST','PAK0001',3,7,3,4);
                     insert into ranking values('SRI LANKA','TEST','SL0001',6,8,5,7);
@@ -178,28 +178,28 @@ create table ranking(
                     
 
 create table venue_scores(	
-					venue_name varchar(30) not null,
-					game_type varchar(4),
-					least_score varchar(10),
-					highest_score varchar(10),
-					least_defend varchar(10),
-					highest_chase varchar(10),
-					boundaries integer(4),
-					primary key (venue_name,game_type)
-					);
+			venue_name varchar(30) not null,
+			game_type varchar(4),
+			least_score varchar(10),
+			highest_score varchar(10),
+			least_defend varchar(10),
+			highest_chase varchar(10),
+			boundaries integer(4),
+			primary key (venue_name,game_type)
+			);
                     insert into venue_scores values('RAJIVGANDHI','ODI','100 by BAN','350 by IND','127 by AUS','342 by IND',89067);
-					insert into venue_scores values('RAJIVGANDHI','TEST','35 by IND','600 by IND','180 by IND','360 by IND',44567);
-					insert into venue_scores values('RAJIVGANDHI','T20','29 by ENG','298 by IND','129 by AUS','198 by SA',44567);
-					insert into venue_scores values('GABBA','TEST','36 by IND','726 by IND','145 by AUS','389 by IND',89067);
-					insert into venue_scores values('GABBA','T20','60 by AUS','196 by AUS','167 by AUS','193 by IND',37689);
+		    insert into venue_scores values('RAJIVGANDHI','TEST','35 by IND','600 by IND','180 by IND','360 by IND',44567);
+		    insert into venue_scores values('RAJIVGANDHI','T20','29 by ENG','298 by IND','129 by AUS','198 by SA',44567);
+		    insert into venue_scores values('GABBA','TEST','36 by IND','726 by IND','145 by AUS','389 by IND',89067);
+		    insert into venue_scores values('GABBA','T20','60 by AUS','196 by AUS','167 by AUS','193 by IND',37689);
                     insert into venue_scores values('LORDS','ODI','100 by NZ','483 by ENG','187 by ENG','342 by ENG',12768);
-					insert into venue_scores values('LORDS','TEST','123 by IND','587 by AUS','187 by NZ','356 by ENG',87956);
-					insert into venue_scores values('LORDS','T20','56 by SA' ,'187 by ENG','157 by PAK','167 by ENG',128796);
-					insert into venue_scores values('WANKADE','ODI','145 by ENG','344 by IND','177 by IND','242 by IND',89067);
+		    insert into venue_scores values('LORDS','TEST','123 by IND','587 by AUS','187 by NZ','356 by ENG',87956);
+		    insert into venue_scores values('LORDS','T20','56 by SA' ,'187 by ENG','157 by PAK','167 by ENG',128796);
+		    insert into venue_scores values('WANKADE','ODI','145 by ENG','344 by IND','177 by IND','242 by IND',89067);
                     
 create table umpire(
                     umpire_id varchar(7) primary key,
-					umpire_name varchar(15) not null,
+		    umpire_name varchar(15) not null,
                     age int(2),
                     Number_of_matches int(3)
                     );
@@ -209,41 +209,41 @@ create table umpire(
                     insert into umpire values('UMP3333','lynn',50,180);
                     
 create table venue_result( 
-						venue_name varchar(30),
-						match_date date,
-						game_type varchar(4),
-						team_1 varchar(15),
-						team_2 varchar(15),
-						result varchar(50),
+			venue_name varchar(30),
+			match_date date,
+			game_type varchar(4),
+			team_1 varchar(15),
+			team_2 varchar(15),
+			result varchar(50),
                         man_of_the_match varchar(7),
-						umpire_by varchar(7),
-						primary key (venue_name,match_date),
-						foreign key (venue_name) references venue_scores(venue_name),
-						foreign key (umpire_by) references umpire(umpire_id)
-						);
-						insert into venue_result values('RAJIVGANDHI','2007-2-13','test','INDIA','ENGLAND','India won by 10 wickets','IND0001','UMP1111');
-						insert into venue_result values('GABBA','2007-2-13','ODI','AUSTRALIA','SOUTH AFRICA','Australia won by 45 runs','AUS0002','UMP2222');
-						insert into venue_result values('WANKADE','2009-12-30','T20','SRILANKA','INDIA','Srilanka won by 3 runs','SL0001','UMP1111');
+			umpire_by varchar(7),
+			primary key (venue_name,match_date),
+			foreign key (venue_name) references venue_scores(venue_name),
+			foreign key (umpire_by) references umpire(umpire_id)
+			);
+			insert into venue_result values('RAJIVGANDHI','2007-2-13','test','INDIA','ENGLAND','India won by 10 wickets','IND0001','UMP1111');
+			insert into venue_result values('GABBA','2007-2-13','ODI','AUSTRALIA','SOUTH AFRICA','Australia won by 45 runs','AUS0002','UMP2222');
+			insert into venue_result values('WANKADE','2009-12-30','T20','SRILANKA','INDIA','Srilanka won by 3 runs','SL0001','UMP1111');
                         insert into venue_result values('LORDS', '2012-12-04','ODI','INDIA','ENGLAND','India won by 5 wickets','IND0001','UMP3333');
                         
 create table council_info(
-						council_name varchar(5) primary key,
-						controlling_country varchar(20),
-						head varchar(20),
-						foreign key (controlling_country) references ranking(country)
-						);
+			council_name varchar(5) primary key,
+			controlling_country varchar(20),
+			head varchar(20),
+			foreign key (controlling_country) references ranking(country)
+			);
                         insert into council_info values('BCCI','INDIA','gangully');
-						insert into council_info values('CA','AUSTRALIA','eddings');
-						insert into council_info values('ECB','ENGLAND','Watmore');
-						insert into council_info values('NZC','NEWZLAND','martin snedden');
-						insert into council_info values('CAS','SOUTHAFRICA','naido');
+			insert into council_info values('CA','AUSTRALIA','eddings');
+			insert into council_info values('ECB','ENGLAND','Watmore');
+			insert into council_info values('NZC','NEWZLAND','martin snedden');
+			insert into council_info values('CAS','SOUTHAFRICA','naido');
                         insert into council_info values('SLC', 'SRI LANKA', 'shammi silva');
                         insert into council_info values('PCB','PAKISTAN','ramiz raja');
                         
 create table played_players(
-							match_date date,
+			    match_date date,
                             country_id varchar(7),
-			                player_id varchar(7),
+			    player_id varchar(7),
                             runs_scored varchar(20),
                             wickets_taken varchar(20),
                             catches_taken integer,
@@ -254,10 +254,10 @@ create table played_players(
                             );
                             insert into played_players values ('2012-12-04','BCCI','IND0001','183 in 150 balls','0 in 0 overs',2,20);
                             insert into played_players values ('2012-12-04','BCCI','IND0003','67 in 40 balls','0 in 0 overs',1,15);
-							insert into played_players values ('2012-12-04','BCCI','IND0002','0 in 0 balls','2 in 9 overs',1,12);
-							insert into played_players values ('2012-12-04','ECB','ENG0001','60 in 50 balls','0 in 0 overs',2,15);
-							insert into played_players values ('2012-12-04','ECB','ENG0002','83 in 70 balls','0 in 0 overs',2,20);
-							insert into played_players values ('2012-12-04','ECB','ENG0003','0 in 0 balls','3 in 9 overs',1,20);
+			    insert into played_players values ('2012-12-04','BCCI','IND0002','0 in 0 balls','2 in 9 overs',1,12);
+			    insert into played_players values ('2012-12-04','ECB','ENG0001','60 in 50 balls','0 in 0 overs',2,15);
+			    insert into played_players values ('2012-12-04','ECB','ENG0002','83 in 70 balls','0 in 0 overs',2,20);
+			    insert into played_players values ('2012-12-04','ECB','ENG0003','0 in 0 balls','3 in 9 overs',1,20);
                             insert into played_players values ('2007-2-13','CA','AUS0001','101 in 68 balls','0 in 0 overs',2,10);
                             insert into played_players values ('2007-2-13','CA','AUS0003','92 in 75 balls','0 in 0 overs',1,15);
                             insert into played_players values ('2007-2-13','CA','AUS0002','0 in 0 balls','2 in 10 overs',1,10);
